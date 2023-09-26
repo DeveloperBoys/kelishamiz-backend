d95ad43f2457020423e11286a0ba1c57caf75b0f
murtazoxurramov <murtazoxurramov1@gmail.com>
2023-09-26 12:33:41 +0500

    fixed bugs in classifieds app

--- apps/classifieds/views.py
+++ apps/classifieds/views.py
@@ -6,9 +6,8 @@ from rest_framework.exceptions import ValidationError
 from rest_framework import generics, permissions, response, pagination
 
 
-from apps.ads.models import TopClassified
 from apps.permissions.permissions import ClassifiedOwnerOrReadOnly, IsAdminOrReadOnly
-from .models import Category, Classified, ClassifiedDetail, ClassifiedImage, DynamicField
+from .models import Category, Classified, ClassifiedImage
 from .serializers import (
     CategorySerializer,
     ClassifiedListSerializer,
@@ -36,14 +35,15 @@ class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
 
 
 class ClassifiedListView(generics.ListCreateAPIView):
-    queryset = Classified.objects.all()
+    queryset = Classified.objects.filter(
+        is_active=True).order_by('-created_at')
     serializer_class = ClassifiedListSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
     http_method_names = ['get', ]
 
 
 class ClassifiedDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Classified.objects.all()
+    queryset = Classified.objects.filter(is_active=True)
     serializer_class = ClassifiedSerializer
     permission_classes = (ClassifiedOwnerOrReadOnly, )
 
@@ -82,35 +82,3 @@ class ClassifiedCreateView(generics.CreateAPIView):
             classified=classified,
             image=f'classifieds/{image_name}'
         )
-
-
-class CombinedClassifiedListView(generics.ListCreateAPIView):
-    serializer_class = ClassifiedListSerializer
-    pagination_class = pagination.LimitOffsetPagination
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def list(self, request, *args, **kwargs):
-        top_classified_ids = TopClassified.objects.filter(
-            is_active=True).values_list('classified_id', flat=True)
-
-        top_classifieds = TopClassified.objects.filter(
-            is_active=True).order_by('classified__created_at')
-
-        regular_classifieds = Classified.objects.filter(is_active=True).exclude(
-            id__in=top_classified_ids).order_by('-created_at')
-
-        # serialize with request context
-        top_serialized = None
-        if top_classifieds.exists():
-            top_serialized = ClassifiedListSerializer(
-                top_classifieds, many=True, context={'request': request})
-
-        regular_serialized = ClassifiedListSerializer(
-            regular_classifieds, many=True, context={'request': request})
-
-        data = {
-            'top_classifieds': top_serialized.data,
-            'regular_classifieds': regular_serialized.data
-        }
-
-        return response.Response(data)

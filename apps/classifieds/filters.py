4f4fc4f03c22e77ccf09ace7aa472d02a0cbbf0f
murtazoxurramov <murtazoxurramov1@gmail.com>
2023-11-13 17:23:48 +0500

    add location filter in classifieds

--- apps/classifieds/filters.py
+++ apps/classifieds/filters.py
@@ -1,5 +1,7 @@
 import django_filters
+
 from .models import Classified, Category
+from apps.site_settings.models import Locations
 
 
 class ClassifiedFilter(django_filters.FilterSet):
@@ -9,7 +11,10 @@ class ClassifiedFilter(django_filters.FilterSet):
         field_name="classifieddetail__price", lookup_expr='lte')
     category = django_filters.ModelChoiceFilter(
         queryset=Category.objects.all())
+    location = django_filters.ModelChoiceFilter(
+        queryset=Locations.objects.all()
+    )
 
     class Meta:
         model = Classified
-        fields = ['min_price', 'max_price', 'category']
+        fields = ['min_price', 'max_price', 'category', 'location']

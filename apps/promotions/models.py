4a1ed7f9b5bb3a573ac82f55459368120c0ac88a
murtazoxurramov <murtazoxurramov1@gmail.com>
2023-11-18 22:27:53 +0500

    add changes and news in promotions app

--- apps/promotions/models.py
+++ apps/promotions/models.py
@@ -1,4 +1,8 @@
+from datetime import timedelta
+
 from django.db import models
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 from apps.classifieds.models import Classified
 
@@ -9,7 +13,7 @@ class Promotion(models.Model):
     views_multiplier = models.IntegerField()
     duration = models.IntegerField(help_text="Duration in days")
     bump_ups = models.IntegerField(help_text="Number of ad bump ups")
-    vip_days = models.IntegerField(help_text="Number of days in VIP ads")
+    vip_duration = models.IntegerField(help_text="Number of days in VIP ads")
 
     def __str__(self):
         return self.name
@@ -18,4 +22,36 @@ class Promotion(models.Model):
 class PromotionClassified(models.Model):
     classified = models.ForeignKey(Classified, on_delete=models.CASCADE)
     promotion = models.ForeignKey(Promotion, on_delete=models.CASCADE)
-    is_active = models.BooleanField(default=False)
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField()
+
+    @property
+    def is_active(self):
+        now = timezone.now()
+        return self.start_date <= now <= self.end_date
+
+    def validate_promotion(self):
+        try:
+            if not self.promotion.is_active:
+                raise ValidationError("Promotion is no longer active")
+        except Promotion.DoesNotExist:
+            raise ValidationError("Invalid promotion")
+
+        # Check user balance
+        if self.classified.user.balance < self.promotion.cost:
+            raise ValidationError("Insufficient balance for promotion cost")
+
+        # Check classified not already promoted
+        other_promos = PromotionClassified.objects.filter(
+            classified=self.classified,
+            is_active=True
+        )
+        if other_promos.exists():
+            raise ValidationError("Classified already has an active promotion")
+
+    def clean(self):
+        self.validate_promotion()
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)

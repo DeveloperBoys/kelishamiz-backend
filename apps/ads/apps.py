7036f81e6180ef6b075c0ff30df313ad9381e0d1
murtazoxurramov <murtazoxurramov1@gmail.com>
2023-11-28 12:26:13 +0500

    change app name from promotions to ads

--- apps/ads/apps.py
+++ apps/ads/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PromotionsConfig(AppConfig):
+class AdsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.promotions'
+    name = 'apps.ads'
--- apps/ads/models.py
+++ apps/ads/models.py
@@ -11,9 +11,21 @@ class Promotion(models.Model):
     name = models.CharField(max_length=100)
     cost = models.DecimalField(max_digits=10, decimal_places=2)
     views_multiplier = models.IntegerField()
-    duration = models.IntegerField(help_text="Duration in days")
     bump_ups = models.IntegerField(help_text="Number of ad bump ups")
-    vip_duration = models.IntegerField(help_text="Number of days in VIP ads")
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class VipPromotion(Promotion):
+    duration = models.IntegerField(help_text="Number of days in VIP ads")
+
+    class Meta:
+        verbose_name = "Vip Promotion"
+        verbose_name_plural = "Vip Promotions"
 
     def __str__(self):
         return self.name

126eac00b6c8203ceeb4ec9a9aa3094c8d4378e1
murtazoxurramov <murtazoxurramov1@gmail.com>
2023-11-28 21:29:11 +0500

    register UserBalance model in admins.py

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import CustomOrder
+from .models import CustomOrder, UserBalance
 from payme.models import MerchatTransactionsModel
 
 admin.site.register(CustomOrder)
+admin.site.register(UserBalance)

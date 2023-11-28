fca561c38dcdb9498e0ca9622aab0d64dfca23ec
murtazoxurramov <murtazoxurramov1@gmail.com>
2023-11-28 21:30:32 +0500

    fix bugs and change logic in models

--- apps/ads/models.py
+++ apps/ads/models.py
@@ -1,70 +1,105 @@
-from datetime import timedelta
-
 from django.db import models
 from django.utils import timezone
 from django.core.exceptions import ValidationError
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
+from apps.payments.models import UserBalance
 from apps.classifieds.models import Classified
 
 
 class Ad(models.Model):
     name = models.CharField(max_length=100)
+    short_description = models.CharField(max_length=250)
     cost = models.DecimalField(max_digits=10, decimal_places=2)
-    views_multiplier = models.IntegerField()
-    bump_ups = models.IntegerField(help_text="Number of ad bump ups")
-    duration = models.IntegerField(help_text="Number of days in VIP ads")
+    bump_ups = models.IntegerField(
+        help_text="Number of ad bump ups", blank=True, null=True)
+    top_duration = models.IntegerField(
+        help_text="Number of days in TOP ads", blank=True, null=True)
 
     class Meta:
         abstract = True
+
+
+class StartAd(Ad):
+
+    class Meta:
+        verbose_name = "Start Ad"
+        verbose_name_plural = "Start Ads"
 
     def __str__(self):
         return self.name
 
 
-# class VipAd(Ad):
-#     duration = models.IntegerField(help_text="Number of days in VIP ads")
+class FasterAd(Ad):
+    bump_ups = models.IntegerField(
+        help_text="Number of ad bump ups")
+    top_duration = models.IntegerField(
+        help_text="Number of days in TOP ads")
 
-#     class Meta:
-#         verbose_name = "Vip Ad"
-#         verbose_name_plural = "Vip Ads"
+    class Meta:
+        verbose_name = "Faster Ad"
+        verbose_name_plural = "Faster Ads"
 
-#     def __str__(self):
-#         return self.name
+    def __str__(self):
+        return self.name
+
+
+class VipAd(Ad):
+    bump_ups = models.IntegerField(
+        help_text="Number of ad bump ups")
+    top_duration = models.IntegerField(
+        help_text="Number of days in TOP ads")
+    vip_duration = models.IntegerField(help_text="Number of days in VIP ads")
+
+    class Meta:
+        verbose_name = "Vip Ad"
+        verbose_name_plural = "Vip Ads"
+
+    def __str__(self):
+        return self.name
 
 
 class AdClassified(models.Model):
     classified = models.ForeignKey(Classified, on_delete=models.CASCADE)
-    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    # ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    ad_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    ad_object_id = models.PositiveIntegerField()
+    ad = GenericForeignKey('ad_content_type', 'ad_object_id')
+
     start_date = models.DateTimeField(auto_now_add=True)
-    end_date = models.DateTimeField()
+    end_date = models.DateTimeField(blank=True, null=True)
 
     @property
     def is_active(self):
         now = timezone.now()
-        return self.start_date <= now <= self.end_date
+        if self.pk:
+            return self.start_date <= now <= self.end_date
+        return False
 
-    def validate_ad(self):
-        try:
-            if not self.ad.is_active:
-                raise ValidationError("Ad is no longer active")
-        except Ad.DoesNotExist:
-            raise ValidationError("Invalid ad")
+    def validate(self):
+        user_balance = UserBalance.objects.filter(
+            user=self.classified.owner).last()
+        if not user_balance or user_balance.balance < self.ad.cost:
+            raise ValidationError("Insufficient balance")
 
-        # Check user balance
-        if self.classified.user.balance < self.ad.cost:
-            raise ValidationError("Insufficient balance for ad cost")
-
-        # Check classified not already promoted
-        other_ads = AdClassified.objects.filter(
+        active_ads = AdClassified.objects.filter(
             classified=self.classified,
-            is_active=True
+            start_date__lte=timezone.now(),
+            end_date__gte=timezone.now()
         )
-        if other_ads.exists():
-            raise ValidationError("Classified already has an active ad")
+        if active_ads.exists():
+            raise ValidationError(
+                "Classified already has an active purchased ad")
 
-    def clean(self):
-        self.validate_ad()
+    def deduct_ad_cost(self):
+        user_balance = UserBalance.objects.filter(
+            user=self.classified.owner).last()
+        user_balance.balance -= self.ad.cost
+        user_balance.save()
 
     def save(self, *args, **kwargs):
-        self.full_clean()
+        if not self.pk:
+            self.validate()
+            self.deduct_ad_cost()
         super().save(*args, **kwargs)

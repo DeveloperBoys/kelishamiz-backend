a9258dbc8f64537fe12f72f54b3c05dfdf0679a5
murtazoxurramov <murtazoxurramov1@gmail.com>
2023-12-01 00:18:22 +0500

    add some changes

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+set -e
 
+cd ./backend/
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
--- docker/celery-worker/entry.sh
+++ docker/celery-worker/entry.sh
@@ -3,8 +3,9 @@ set -e
 
 # Run tests
 cd ./backend/
+python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --clear --noinput
 
 echo "Starting celery worker"
-exec celery -A backend worker --beat -Q celery --loglevel=info --concurrency=1 --max-memory-per-child=25000 --scheduler django_celery_beat.schedulers:DatabaseScheduler
+exec celery -A kelishamiz worker --beat -Q celery --loglevel=info --concurrency=1 --max-memory-per-child=25000 --scheduler django_celery_beat.schedulers:DatabaseScheduler
